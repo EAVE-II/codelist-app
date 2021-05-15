5990ce79e51b11a78918f1eac38db502f149bb55
unknown <stevenkerr2@gmail.com>
2021-05-15 13:13:36 +0100

    _

--- code/analysis.R
+++
@@ -1,7 +0,0 @@
-######################################################################
-## Title: [Insert full title of paper]
-## Short title: [Insert short title of paper (if applicable) here]
-## DOI: [Insert DOI of paper here]
-## Code author: [Insert lead analyst name <lead analyst email> here]
-## Description: [Insert quick description on the code (e.g. Analysis code, data cleansing code) here]
-######################################################################
---
+++ code/app.R
@@ -0,0 +1,159 @@
+library(shiny)
+library(data.table)
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(DT)
+library(shinythemes)
+
+########################## PREPARE DATA #####################################
+
+df <-read_excel("Adverse events - study design and codes_vB.xlsx", sheet = 'Codes')
+
+df <- df[-7]
+
+# cols1 is names of column in the main dataframe
+# cols1View is how the column names will appear in the web app
+cols1 <- c('Category', 'AE', 'viewCodes')
+cols1View <- c('Category', 'Adverse event', 'Codes')
+
+#rename columns
+df <- rename(df,  'AE' = 'Adverse event',
+             'readCodes' = 'Read codes',
+             'icd10Codes' = 'ICD-10 codes',
+             'readCodesSource' =  'Source: Read codes',
+             'icd10CodesSource' = 'Source: ICD-10 code')
+
+df <- fill(df, c('Category', 'AE', 'readCodesSource', 'icd10CodesSource'), .direction = 'down')
+
+df <- group_by(df, AE) %>% mutate( readCodes = list(readCodes),
+                                   icd10Codes = list(icd10Codes)) %>% unique()
+
+df <- as.data.frame(df)
+
+# This makes a series of action buttons
+shinyInput <- function(FUN, len, id, ...) {
+  inputs <- character(len)
+  for (i in seq_len(len)) {
+    inputs[i] <- as.character(FUN(paste0(id, i), ...))
+  }
+  inputs
+}
+
+################################# UI ####################################
+
+ui <- fluidPage(
+  theme = shinytheme("cerulean"),
+  titlePanel('Codes'),
+  uiOutput("header"),
+  uiOutput('table1'),
+  uiOutput('table2'),
+  uiOutput('buttons')
+)
+
+############################## SERVER #####################################
+
+server <- function(input, output) {
+  
+  # Create main table    
+  output$data <- renderDataTable({
+    
+    data <- mutate(df, viewCodes =
+                     shinyInput(actionButton, nrow(df), 'button_', label = "View", class = "btn-primary",
+                                onclick = 'Shiny.onInputChange(\"select_button\",  this.id)' )) %>% select(cols1)
+    
+    
+    table <- datatable(data[cols1], escape = FALSE, selection = 'none',
+                       colnames = cols1View,  filter = 'top',
+                       options = list(columnDefs = list( list(targets = 3, searchable = FALSE),
+                                                         list(width = '50px', targets = 3) )))
+    
+    table
+  })
+  
+  # Render main table in UI
+  output$table1 <- renderUI({DT::dataTableOutput("data")})
+  
+  # This is what happens when you press one of the "View" action buttons in
+  # the main table
+  observeEvent(input$select_button, {
+    
+    # This is the row of the action button that was clicked
+    selectedRow <- as.numeric(strsplit(input$select_button, "_")[[1]][2])
+    
+    # Create data frames with all codes, and read/ICD-10 codes separately
+    allCodes <- data.frame(readCodes = unlist(df[selectedRow, 'readCodes']),
+                           icd10Codes = unlist(df[selectedRow, 'icd10Codes']))
+    
+    allCodes[] <- lapply(allCodes, as.character)
+    
+    readCodes <- na.omit(allCodes['readCodes'])
+    
+    icd10Codes <- na.omit(allCodes['icd10Codes'])
+    
+    # Replace NA with space, for download
+    allCodes[ is.na(allCodes)] <- ''
+    
+    # Category ad adverse event names
+    Cat <- df[selectedRow, 'Category']
+    AE <- df[selectedRow, 'AE']
+    
+    # Render header to UI
+    output$header <- renderUI({
+      tagList(tags$h4( paste("Category:", Cat, sep=" ")),
+              tags$h4("Adverse Event:", AE, sep=" "))
+    })
+    
+    
+    # Create the read code and icd-10 code tables
+    output$readCodes= DT::renderDataTable(
+      readCodes, colnames = 'Read Codes',
+      server = FALSE)
+    
+    output$icd10Codes= DT::renderDataTable(
+      icd10Codes, colnames = 'ICD-10 codes',
+      server = FALSE)
+    
+    # Render read code and icd-10 code tables to UI
+    output$table1 <- renderUI({
+      div(style="padding: 30px 0px", DT::dataTableOutput("icd10Codes"))
+    })
+    
+    output$table2 <- renderUI({
+      div(style="padding: 30px 0px", DT::dataTableOutput("readCodes"))})
+    
+    
+    # This creates a download link button
+    output$download <- downloadHandler(
+      filename = function() {paste( AE, "_codes_", Sys.Date(), '.csv', sep='')},
+      content = function(file) {write.csv(allCodes, file)}
+    )
+    
+    # Render back and download buttons to UI
+    output$buttons <- renderUI({
+      tagList( actionButton("return", "Back to all codes", class = "btn-primary"),
+               downloadButton("download", "Download CSV", class = "btn-primary"))
+    })
+    
+  })
+  
+  # This is what happens when you click the "Back to all codes" button.  
+  observeEvent(input$return, {
+    
+    # Get rid of header
+    output$header <- renderUI({ })
+    
+    # Render main table in UI
+    output$table1 <- renderUI({DT::dataTableOutput("data")})
+    
+    # Get rid of table2
+    output$table2 <- renderUI({})
+    
+    # Get rid of buttons
+    output$buttons <- renderUI({})
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
